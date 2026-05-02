6a36d7501f9e20c066fba7c1722fa5ff05ce9570
PriyadharshiniKannan-22 <priyadharshinikannan22@gmail.com>
2026-05-02 23:53:28 +0530

    Refactored QA logic into separate module to avoid circular dependency

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
-from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
+from utils.loader import load_faq
+from utils.splitter import split_docs
+from utils.vectorstore import create_vectorstore
+from backend.qa_chain import build_qa
 
-def build_qa(db):
-    retriever = db.as_retriever()
-    qa = RetrievalQA.from_chain_type(
-        llm=OpenAI(),
-        retriever=retriever
-    )
-    return qa
+docs = load_faq("data/faq.json")
+split_docs = split_docs(docs)
+db = create_vectorstore(split_docs)
+qa = build_qa(db)
+
+def ask_question(query):
+    return qa.run(query)
